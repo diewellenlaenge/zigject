59ec521229eddb01f998d4d596a92d2d2f1e66f4
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2022-04-15 13:33:35 +0200

    rename to inject.zig

--- test/injecter.zig
+++ test/injecter.zig
@@ -4,5 +4,5 @@ const zigject = @import("zigject");
 pub fn main() anyerror!void {
     const notepad = 19300;
     const dll = "C:\\Users\\Admin\\Coding\\diewellenlaenge\\zigject\\zig-out\\lib\\zigject-injectee.dll";
-    _ = try zigject.RemoteThread(notepad, dll);
+    _ = try zigject.inject.RemoteThread(notepad, dll);
 }
