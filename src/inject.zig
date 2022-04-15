16f4de3f4b21aeb3d2a613466bd11dbd723e072a
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2022-04-15 13:34:02 +0200

    change RemoteThreadError to InjectError

--- src/inject.zig
+++ src/inject.zig
@@ -10,7 +10,7 @@ const debug = win32.system.diagnostics.debug;
 const loader = win32.system.library_loader;
 const foundation = win32.foundation;
 
-pub const RemoteThreadError = error {
+pub const InjectError = error {
     OpenProcess,
     VirtualAllocEx,
     WriteProcessMemory,
@@ -20,27 +20,27 @@ pub const RemoteThreadError = error {
     utf8ToUtf16LeWithNull,
 };
 
-pub fn RemoteThread(pid: win.DWORD, path: []const u8) RemoteThreadError!bool {
-    const process = threading.OpenProcess(threading.PROCESS_ALL_ACCESS, win.FALSE, pid) orelse return RemoteThreadError.OpenProcess;
+pub fn RemoteThread(pid: win.DWORD, path: []const u8) InjectError!bool {
+    const process = threading.OpenProcess(threading.PROCESS_ALL_ACCESS, win.FALSE, pid) orelse return InjectError.OpenProcess;
     defer _ = win32.foundation.CloseHandle(process);
 
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     var galloc = gpa.allocator();
 
-    const pathW = std.unicode.utf8ToUtf16LeWithNull(galloc, path) catch return RemoteThreadError.utf8ToUtf16LeWithNull;
+    const pathW = std.unicode.utf8ToUtf16LeWithNull(galloc, path) catch return InjectError.utf8ToUtf16LeWithNull;
     defer galloc.free(pathW);
     const pathSize: usize = (pathW.len + 1) * @sizeOf(std.meta.Child(@TypeOf(pathW)));
 
-    const buffer = memory.VirtualAllocEx(process, null, pathSize, memory.MEM_COMMIT, memory.PAGE_READWRITE) orelse return RemoteThreadError.VirtualAllocEx;
+    const buffer = memory.VirtualAllocEx(process, null, pathSize, memory.MEM_COMMIT, memory.PAGE_READWRITE) orelse return InjectError.VirtualAllocEx;
     defer _ = memory.VirtualFreeEx(process, buffer, 0, memory.MEM_RELEASE);
 
     if (debug.WriteProcessMemory(process, buffer, @ptrCast(*const anyopaque, pathW), pathSize, null) == win.FALSE) {
-        return RemoteThreadError.WriteProcessMemory;
+        return InjectError.WriteProcessMemory;
     }
 
-    const kernel32 = loader.GetModuleHandleW(L("kernel32.dll")) orelse return RemoteThreadError.GetModuleHandleW;
-    const loadLibrary = loader.GetProcAddress(kernel32, "LoadLibraryW") orelse return RemoteThreadError.GetProcAddress;
-    const thread = threading.CreateRemoteThread(process, null, 0, @ptrCast(threading.LPTHREAD_START_ROUTINE, loadLibrary), buffer, 0, null) orelse return RemoteThreadError.CreateRemoteThread;
+    const kernel32 = loader.GetModuleHandleW(L("kernel32.dll")) orelse return InjectError.GetModuleHandleW;
+    const loadLibrary = loader.GetProcAddress(kernel32, "LoadLibraryW") orelse return InjectError.GetProcAddress;
+    const thread = threading.CreateRemoteThread(process, null, 0, @ptrCast(threading.LPTHREAD_START_ROUTINE, loadLibrary), buffer, 0, null) orelse return InjectError.CreateRemoteThread;
 
     _ = threading.WaitForSingleObject(thread, win.INFINITE);
 
