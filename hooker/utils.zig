538fc9d3289d7183a1d4df0f11f80c83a9bfdd87
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2022-04-16 23:24:58 +0200

    change generate_fn_type to be more specific

--- hooker/utils.zig
+++ hooker/utils.zig
@@ -1,6 +1,6 @@
-const builtin = @import("builtin");
+const std = @import("std");
 
-pub fn generate_fn_type(comptime fn_type: anytype, comptime calling_convention: anytype) type {
+pub fn generate_fn_type(comptime fn_type: anytype, comptime calling_convention: std.builtin.CallingConvention) type {
     var ti = @typeInfo(@TypeOf(fn_type));
     ti.Fn.calling_convention = calling_convention;
     return @Type(ti);
