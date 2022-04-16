0f5e14bcb367e520046291b7abe451363b073ec0
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2022-04-16 23:19:31 +0200

    change generate_type_fn

--- hooker/utils.zig
+++ hooker/utils.zig
@@ -1,7 +1,7 @@
 const builtin = @import("builtin");
 
-pub fn generate_fn_type(comptime fn_type: anytype, comptime calling_convention: builtin.CallingConvention) type {
-    var construct_fn = @typeInfo(@TypeOf(fn_type)).Fn;
-    construct_fn.calling_convention = calling_convention;
-    return @Type(construct_fn);
+pub fn generate_fn_type(comptime fn_type: anytype, comptime calling_convention: anytype) type {
+    var ti = @typeInfo(@TypeOf(fn_type));
+    ti.Fn.calling_convention = calling_convention;
+    return @Type(ti);
 }
