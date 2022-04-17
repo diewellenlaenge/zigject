7f9daa1f2c9861827a830abef6486844fd83e6b5
Sora <dream@neoncity.dev>
2022-04-17 23:40:55 +0200

    Rollback

--- hooker/utils.zig
+++ hooker/utils.zig
@@ -1,11 +1,7 @@
-const callcon = @import("builtin").CallingConvention;
+const std = @import("std");
 
-pub fn generate_fn_type(comptime calling_convention: anytype) type {
-    return fn (p1: u32, p2: u32, p3: u32, p4: u32, p5: u32) callconv(calling_convention) u32;
+pub fn generate_fn_type(comptime fn_type: anytype, comptime calling_convention: std.builtin.CallingConvention) type {
+    var ti = @typeInfo(@TypeOf(fn_type));
+    ti.Fn.calling_convention = calling_convention;
+    return @Type(ti);
 }
-
-//pub fn generate_fn_type(comptime fn_type: anytype, comptime calling_conv: callcon) type {
-//    var construct_fn = @typeInfo(@TypeOf(fn_type)).Fn;
-//    construct_fn.calling_convention = calling_conv;
-//    return @Type(construct_fn);
-//}
